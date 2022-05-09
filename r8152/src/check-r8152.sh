7a2de7876524cc86cbfdcfc89b14cf654dcea47d
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2022-05-09 23:40:41 +0900

    test ifconfig eth1 up

--- r8152/src/check-r8152.sh
+++ r8152/src/check-r8152.sh
@@ -3,8 +3,9 @@
 #
 
 echo -n "Loading module r8152 -> "
-
-if [ `/sbin/lsmod |grep -i r8152|wc -l` -gt 0 ] ; then
-        echo "Module r8152 loaded succesfully"
-        else echo "Module r8152 is not loaded "
-fi
+echo `/sbin/lsmod |grep -i r8152|wc -l`
+ehoo `sudo ifconfig eth1 up`
+#if [ `/sbin/lsmod |grep -i r8152|wc -l` -gt 0 ] ; then
+#        echo "Module r8152 loaded succesfully"
+#        else echo "Module r8152 is not loaded "
+#fi
