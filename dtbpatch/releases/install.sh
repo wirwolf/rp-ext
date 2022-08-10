ca380bb9ef2d1f03a1e0ee96286ca15049dc338b
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2022-08-10 17:18:30 +0900

    Update install.sh

--- dtbpatch/releases/install.sh
+++ dtbpatch/releases/install.sh
@@ -5,17 +5,22 @@
 # probably certain that system has finish init process
 #
 
+# Detect correct file
+HW_REVISION=`cat /proc/sys/kernel/syno_hw_revision`
+[ -n "${HW_REVISION}" ] && DTBFILE="model_${HW_REVISION}.dtb" || DTBFILE="model.dtb"
+[ -e /etc.defaults/${DTBFILE} ] || DTBFILE="model.dtb"
 
 if [ `mount | grep tmpRoot | wc -l` -gt 0 ] ; then
-HASBOOTED="yes"
-echo "System passed junior"
+  HASBOOTED="yes"
+  echo "System passed junior"
 else
-echo "System is booting"
-HASBOOTED="no"
+  echo "System is booting"
+  HASBOOTED="no"
 fi
 
 
 if [ "$HASBOOTED" = "no" ]; then
+
   echo "dtbpatch - early"
   # fix executable flag
   cp dtbpatch /usr/sbin/
@@ -23,21 +28,38 @@ if [ "$HASBOOTED" = "no" ]; then
   chmod +x /usr/sbin/dtbpatch
   chmod +x /usr/sbin/dtc
 
-  # Dynamic generation
-  /usr/sbin/dtbpatch /etc.defaults/model.dtb output.dtb
-  if [ $? -ne 0 ]; then
-    echo "Error patching dtb"
+  echo "Patching /etc.defaults/${DTBFILE}"
+
+  # Dynamic generation fabio
+  if dtbpatch /etc.defaults/${DTBFILE} /var/run/model.dtb; then
+    cp -f /var/run/model.dtb /etc.defaults/${DTBFILE}
   else
-    cp -vf output.dtb /etc.defaults/model.dtb
-    cp -vf output.dtb /var/run/model.dtb
-    /usr/sbin/dtc -I dtb -O dts /etc.defaults/model.dtb > /etc.defaults/model.dts
+    echo "Error patching dtb"
   fi
+
+  # Dynamic generation pocopico
+#  /usr/sbin/dtbpatch /etc.defaults/model.dtb output.dtb
+#  if [ $? -ne 0 ]; then
+#    echo "Error patching dtb"
+#  else
+#    cp -vf output.dtb /etc.defaults/model.dtb
+#    cp -vf output.dtb /var/run/model.dtb
+#    /usr/sbin/dtc -I dtb -O dts /etc.defaults/model.dtb > /etc.defaults/model.dts
+#  fi
+
 elif [ "$HASBOOTED" = "yes" ]; then
   echo "dtbpatch - late"
+  
+  echo "Copying /etc.defaults/${DTBFILE}"
+  
   # copy utilities 
   cp /usr/sbin/dtbpatch /tmpRoot/usr/sbin
   cp /usr/sbin/dtc /tmpRoot/usr/sbin
-  # copy file
-  cp -vf /etc.defaults/model.dtb /tmpRoot/etc.defaults/model.dtb
-  cp -vf /etc.defaults/model.dtb /var/run/model.dtb
+  
+  # copy file fabio
+  cp -vf /etc.defaults/${DTBFILE} /tmpRoot/etc.defaults/model.dtb  
+  
+  # copy file pocopico
+  #cp -vf /etc.defaults/model.dtb /tmpRoot/etc.defaults/model.dtb
+  #cp -vf /etc.defaults/model.dtb /var/run/model.dtb
 fi
