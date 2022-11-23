6aa1436ce9037509a59cd8209219cb99d1cda0f9
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2022-11-23 20:43:59 +0900

    Update tcrp-diagd.sh

--- tcrp-diag/src/tcrp-diagd.sh
+++ tcrp-diag/src/tcrp-diagd.sh
@@ -2,32 +2,32 @@
 
 # You can force junior by adding force_junior in the linux cmdline 
 
-function preparediag(){
+#function preparediag(){
 
-echo "Copying tcrp auxiliary files to /sbin/"
-/bin/cp -v lsscsi /usr/sbin/ ; chmod 700 /usr/sbin/lsscsi
-/bin/cp -v lspci  /usr/sbin/  ; chmod 700 /usr/sbin/lspci
-/bin/cp -v lsusb  /usr/sbin/  ; chmod 700 /usr/sbin/lsusb
-/bin/cp -v dmidecode /usr/sbin/  ; chmod 700 /usr/sbin/dmidecode
-/bin/cp -v dtc /usr/sbin/  ; chmod 700 /usr/sbin/dtc
-/bin/cp -v ethtool /usr/sbin/  ; chmod 700 /usr/sbin/ethtool
-/bin/cp -v tcrp-diag.sh /usr/sbin/  ; chmod 700 /usr/sbin/tcrp-diag.sh
+#echo "Copying tcrp auxiliary files to /sbin/"
+#/bin/cp -v lsscsi /usr/sbin/ ; chmod 700 /usr/sbin/lsscsi
+#/bin/cp -v lspci  /usr/sbin/  ; chmod 700 /usr/sbin/lspci
+#/bin/cp -v lsusb  /usr/sbin/  ; chmod 700 /usr/sbin/lsusb
+#/bin/cp -v dmidecode /usr/sbin/  ; chmod 700 /usr/sbin/dmidecode
+#/bin/cp -v dtc /usr/sbin/  ; chmod 700 /usr/sbin/dtc
+#/bin/cp -v ethtool /usr/sbin/  ; chmod 700 /usr/sbin/ethtool
+#/bin/cp -v tcrp-diag.sh /usr/sbin/  ; chmod 700 /usr/sbin/tcrp-diag.sh
 
-echo "Copying tcrp libraries to /lib/"
-/bin/cp -v libpci.so.3 /lib ; chmod 644 /lib/libpci.so.3
-/bin/cp -v libusb-1.0.so.0 /lib  ; chmod 644 /lib/libusb-1.0.so.0
-/bin/cp -v libz.so.1      /lib  ; chmod 644 /lib/libz.so.1     
-/bin/cp -v libudev.so.1   /lib  ; chmod 644 /lib/libudev.so.1  
-/bin/cp -v libkmod.so.2   /lib  ; chmod 644 /lib/libkmod.so.2  
-/bin/cp -v libresolv.so.2 /lib  ; chmod 644 /lib/libresolv.so.2
-/bin/mkdir /var/lib/usbutils ; /bin/cp usb.ids /var/lib/usbutils/usb.ids ;  chmod 644 /var/lib/usbutils/usb.ids
+#echo "Copying tcrp libraries to /lib/"
+#/bin/cp -v libpci.so.3 /lib ; chmod 644 /lib/libpci.so.3
+#/bin/cp -v libusb-1.0.so.0 /lib  ; chmod 644 /lib/libusb-1.0.so.0
+#/bin/cp -v libz.so.1      /lib  ; chmod 644 /lib/libz.so.1     
+#/bin/cp -v libudev.so.1   /lib  ; chmod 644 /lib/libudev.so.1  
+#/bin/cp -v libkmod.so.2   /lib  ; chmod 644 /lib/libkmod.so.2  
+#/bin/cp -v libresolv.so.2 /lib  ; chmod 644 /lib/libresolv.so.2
+#/bin/mkdir /var/lib/usbutils ; /bin/cp usb.ids /var/lib/usbutils/usb.ids ;  chmod 644 /var/lib/usbutils/usb.ids
 
-}
+#}
 
 
-function getvars(){
+#function getvars(){
 
-    let HEAD=1
+#    let HEAD=1
 
     #if [ -n "$(grep tcrpdiag /proc/cmdline)" ]; then
     #TCRPDIAG="enabled"
@@ -39,20 +39,21 @@ function getvars(){
     # probably certain that system has finish init process 
     #
 
-    if [ `ps -ef |grep -i scemd |grep -v grep | wc -l` -gt 0 ] ; then 
-        HASBOOTED="yes"
-        echo "System has completed init process"
-    else 
-        echo "System is booting"
-        HASBOOTED="no"
-    fi
+#    if [ `ps -ef |grep -i scemd |grep -v grep | wc -l` -gt 0 ] ; then 
+#        HASBOOTED="yes"
+#        echo "System has completed init process"
+#    else 
+#        echo "System is booting"
+#        HASBOOTED="no"
+#    fi
 
-}
+#}
 
 
 ############ START RUN ############
-getvars
+#getvars
 echo "TCRP DIAGD START !!!!!!" 	
+/usr/sbin/tcrp-diag.sh 
 
 #wait_time=5 # maximum wait time in seconds
 #time_counter=0
@@ -63,9 +64,9 @@ echo "TCRP DIAGD START !!!!!!"
 
 #if [ $(ifconfig eth0 | grep inet | wc -l) -eq 0 ]; then
 
-    if  [ "$HASBOOTED" = "no" ] ; then
-        preparediag       
-    fi
+#    if  [ "$HASBOOTED" = "no" ] ; then
+#        preparediag       
+#    fi
     
  #   wait_time=50 # maximum wait time in seconds
  #   time_counter=0
@@ -74,11 +75,8 @@ echo "TCRP DIAGD START !!!!!!"
  #     echo "Still waiting for all devices to write logs (waited $((time_counter=time_counter+1)) of ${wait_time} seconds)"
  #   done
 
-    /usr/sbin/tcrp-diag.sh 
+#    /usr/sbin/tcrp-diag.sh 
 
-#    if [ "$HASBOOTED" = "yes" ] ; then
-        startcollection
-#    fi
 #fi    
 
 exit 0
