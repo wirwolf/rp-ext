54f1933ef63378ca2ca58d305c534bd77ddde201
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-01-10 20:10:10 +0900

    Update install-nvme-cache.sh

--- nvme-cache/src/install-nvme-cache.sh
+++ nvme-cache/src/install-nvme-cache.sh
@@ -1,7 +1,14 @@
   echo "Installing NVMe cache enabler tools"
   cp -vf nvme-cache.sh /tmpRoot/usr/sbin/nvme-cache.sh
   chmod 755 /tmpRoot/usr/sbin/nvme-cache.sh
-  cp -vf nvme-cache.service /tmpRoot/etc/systemd/system/nvme-cache.service
-  chmod 755 /tmpRoot/etc/systemd/system/nvme-cache.service
+  cat > /tmpRoot/etc/systemd/system/nvme-cache.service <<'EOF'
+[Unit]
+Description=NVMe cache enabler schedule
+[Service]
+Type=oneshot
+ExecStart=/usr/sbin/nvme-cache.sh
+[Install]
+WantedBy=multi-user.target
+EOF
   mkdir -p /tmpRoot/etc/systemd/system/multi-user.target.wants
   ln -sf /etc/systemd/system/nvme-cache.service /tmpRoot/etc/systemd/system/multi-user.target.wants/nvme-cache.service
